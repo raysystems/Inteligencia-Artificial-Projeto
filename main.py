154bb4c314341b4ab02eb9934759459a0396b0ae
Ray Systems <md.ramosf09@gmail.com>
2024-11-01 15:18:42 +0000

    Initial Commit
    Initial Commit

---
+++ main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from modules.andar import  frente, tras, direita, esquerda
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+# Robot começa no 1,1
+posicao = [1,1,1]
+# 0 - x
+# 1 - y
+#    
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+#DEFINIÃO MOTOR
+
+
+
+gyro = GyroSensor(Port.S4)
+
+gyro.reset_angle(0)
+
+motor_Direita = Motor(Port.A)
+
+motor_Esquerda = Motor(Port.D)
+
+sensor_Cor = ColorSensor(Port.S2)
+
+
+
+#direita(motor_Esquerda, motor_Direita, gyro)
+
+
+#frente(motor_Esquerda, motor_Direita)
+
+#direita(motor_Esquerda, motor_Direita, gyro, posicao, sensor_Cor)
+
+frente(motor_Esquerda, motor_Direita, posicao, sensor_Cor)
+ev3.speaker.beep()
+wait(2000)
+
+frente(motor_Esquerda, motor_Direita, posicao, sensor_Cor)
+ev3.speaker.beep()
+wait(2000)
+
+direita(motor_Esquerda, motor_Direita, gyro, posicao, sensor_Cor)
+ev3.speaker.beep()
+wait(2000)
+
+esquerda(motor_Esquerda, motor_Direita, gyro, posicao, sensor_Cor)
+ev3.speaker.beep()
+wait(2000)
+
+frente(motor_Esquerda, motor_Direita, posicao, sensor_Cor)
+ev3.speaker.beep()
+wait(2000)
+
+frente(motor_Esquerda, motor_Direita, posicao, sensor_Cor)
+ev3.speaker.beep()
+wait(2000)
+
+
+frente(motor_Esquerda, motor_Direita, posicao, sensor_Cor)
+ev3.speaker.beep()
+wait(2000)
+
+
+frente(motor_Esquerda, motor_Direita, posicao, sensor_Cor)
+ev3.speaker.beep()
+wait(2000)
+
+# esquerda(motor_Esquerda, motor_Direita, gyro, posicao, sensor_Cor)
+# ev3.speaker.beep()
+# wait(2000)
+
+# direita(motor_Esquerda, motor_Direita, gyro, posicao, sensor_Cor)
+# ev3.speaker.beep()
+# wait(2000)
+
+
+#beep
+
+
---
+++ modules/andar.py
@@ -0,0 +1,209 @@
+from pybricks.parameters import Stop
+from pybricks.tools import DataLog, wait, StopWatch
+from pybricks.parameters import Color
+
+def trocarOrientacao(direcao, posicao):
+    if direcao == "E":
+        posicao[2] = 4 if posicao[2] - 1 < 1 else posicao[2] - 1
+    elif direcao == "D":
+        posicao[2] = 1 if posicao[2] + 1 > 4 else posicao[2] + 1
+
+def direita(motore, motord, gyro, posicao, sensor_cor):
+    if posicao[2] == 1 and posicao[0] <= 5:
+        while gyro.angle() <= 90:
+            motore.run(-250)
+            motord.run(250)
+        gyro.reset_angle(0)
+        motore.brake()
+        motord.brake()
+        while sensor_cor.color() != Color.BLACK:
+            motore.run(250)
+            motord.run(250)
+        wait(2000)
+        motore.brake()
+        motord.brake()
+        posicao[0] += 1
+        trocarOrientacao("D", posicao)
+    elif posicao[2] == 2 and posicao[1] > 1:
+        while gyro.angle() <= 90:
+            motore.run(-250)
+            motord.run(250)
+        gyro.reset_angle(0)
+        motore.brake()
+        motord.brake()
+        while sensor_cor.color() != Color.BLACK:
+            motore.run(250)
+            motord.run(250)
+        wait(2000)
+        motore.brake()
+        motord.brake()
+        posicao[1] -= 1
+        trocarOrientacao("D", posicao)
+    elif posicao[2] == 3 and posicao[0] > 1:
+        while gyro.angle() <= 90:
+            motore.run(-250)
+            motord.run(250)
+        gyro.reset_angle(0)
+        motore.brake()
+        motord.brake()
+        while sensor_cor.color() != Color.BLACK:
+            motore.run(250)
+            motord.run(250)
+        wait(2000)
+        motore.brake()
+        motord.brake()
+        posicao[0] -= 1
+        trocarOrientacao("D", posicao)
+    elif posicao[2] == 4 and posicao[1] <= 5:
+        while gyro.angle() <= 90:
+            motore.run(-250)
+            motord.run(250)
+        gyro.reset_angle(0)
+        motore.brake()
+        motord.brake()
+        while sensor_cor.color() != Color.BLACK:
+            motore.run(250)
+            motord.run(250)
+        wait(2000)
+        motore.brake()
+        motord.brake()
+        posicao[1] += 1
+        trocarOrientacao("D", posicao)
+
+def esquerda(motore, motord, gyro, posicao, sensor_cor):
+    if posicao[2] == 1 and posicao[0] > 1:
+        while gyro.angle() >= -90:
+            motore.run(250)
+            motord.run(-250)
+        gyro.reset_angle(0)
+        motore.brake()
+        motord.brake()
+        while sensor_cor.color() != Color.BLACK:
+            motore.run(250)
+            motord.run(250)
+        wait(2000)
+        motore.brake()
+        motord.brake()
+        posicao[0] -= 1
+        trocarOrientacao("E", posicao)
+    elif posicao[2] == 2 and posicao[1] <= 5:
+        while gyro.angle() >= -90:
+            motore.run(250)
+            motord.run(-250)
+        gyro.reset_angle(0)
+        motore.brake()
+        motord.brake()
+        while sensor_cor.color() != Color.BLACK:
+            motore.run(250)
+            motord.run(250)
+        wait(2000)
+        motore.brake()
+        motord.brake()
+        posicao[1] += 1
+        trocarOrientacao("E", posicao)
+    elif posicao[2] == 3 and posicao[0] <= 5:
+        while gyro.angle() >= -90:
+            motore.run(250)
+            motord.run(-250)
+        gyro.reset_angle(0)
+        motore.brake()
+        motord.brake()
+        while sensor_cor.color() != Color.BLACK:
+            motore.run(250)
+            motord.run(250)
+        wait(2000)
+        motore.brake()
+        motord.brake()
+        posicao[0] += 1
+        trocarOrientacao("E", posicao)
+    elif posicao[2] == 4 and posicao[1] > 1:
+        while gyro.angle() >= -90:
+            motore.run(250)
+            motord.run(-250)
+        gyro.reset_angle(0)
+        motore.brake()
+        motord.brake()
+        while sensor_cor.color() != Color.BLACK:
+            motore.run(250)
+            motord.run(250)
+        wait(2000)
+        motore.brake()
+        motord.brake()
+        posicao[1] -= 1
+        trocarOrientacao("E", posicao)
+
+def frente(motore, motord, posicao, sensor_cor):
+    if posicao[2] == 1 and posicao[1] <= 5:
+        motore.run(250)
+        motord.run(250)
+        while sensor_cor.color() != Color.BLACK:
+            pass
+        wait(1500)
+        motore.brake()
+        motord.brake()
+        posicao[1] += 1
+    elif posicao[2] == 2 and posicao[0] <= 5:
+        motore.run(250)
+        motord.run(250)
+        while sensor_cor.color() != Color.BLACK:
+            pass
+        wait(1500)
+        motore.brake()
+        motord.brake()
+        posicao[0] += 1
+    elif posicao[2] == 3 and posicao[1] > 1:
+        motore.run(250)
+        motord.run(250)
+        while sensor_cor.color() != Color.BLACK:
+            pass
+        wait(1500)
+        motore.brake()
+        motord.brake()
+        posicao[1] -= 1
+    elif posicao[2] == 4 and posicao[0] > 1:
+        motore.run(250)
+        motord.run(250)
+        while sensor_cor.color() != Color.BLACK:
+            pass
+        wait(1500)
+        motore.brake()
+        motord.brake()
+        posicao[0] -= 1
+
+def tras(motore, motord, posicao, sensor_cor):
+    if posicao[2] == 1 and posicao[1] > 1:
+        motore.run(-250)
+        motord.run(-250)
+        while sensor_cor.color() != Color.BLACK:
+            pass
+        wait(500)
+        motore.brake()
+        motord.brake()
+        posicao[1] -= 1
+    elif posicao[2] == 2 and posicao[0] > 1:
+        motore.run(-250)
+        motord.run(-250)
+        while sensor_cor.color() != Color.BLACK:
+            pass
+        wait(500)
+        motore.brake()
+        motord.brake()
+        posicao[0] -= 1
+    elif posicao[2] == 3 and posicao[1] <= 5:
+        motore.run(-250)
+        motord.run(-250)
+        while sensor_cor.color() != Color.BLACK:
+            pass
+        wait(500)
+        motore.brake()
+        motord.brake()
+        posicao[1] += 1
+    elif posicao[2] == 4 and posicao[0] <= 5:
+        motore.run(-250)
+        motord.run(-250)
+        while sensor_cor.color() != Color.BLACK:
+            pass
+        wait(500)
+        motore.brake()
+        motord.brake()
+        posicao[0] += 1
